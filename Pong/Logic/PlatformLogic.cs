d07da08a8485b73d34b5fef89cc916c3d7e742d5
TKornél023 <tkornel23@gmail.com>
2022-04-05 12:19:16 +0200

    asd

--- Pong/Logic/PlatformLogic.cs
+++ Pong/Logic/PlatformLogic.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Pong.Logic
 {
-    public class PlatformLogic
+    public class PlatformLogic : IGameModel
     {
+        public double Angle { get; set; }
+        public Ball Ball { get; set; }
+        public Platform Platform { get; set; }
+
+        public event EventHandler Changed;
+        public event EventHandler GameOver;
+
+        System.Windows.Size area;
+
+        public void SetupSizes(System.Windows.Size area)
+        {
+            this.area = area;
+            Ball = new Ball(new System.Windows.Size(area.Width, area.Height));
+            Platform = new Platform();
+        }
     }
 }
