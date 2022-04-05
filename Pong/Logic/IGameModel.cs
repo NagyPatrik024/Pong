85c1b65fc40bbc8c4e0fe0b12263cc8fd79abb08
TKornél023 <tkornel23@gmail.com>
2022-04-05 12:13:42 +0200

    IGameModel interaface

---
+++ Pong/Logic/IGameModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong.Logic
+{
+    internal interface IGameModel
+    {
+        double Angle { get; set; }
+        event EventHandler Changed;
+
+        Ball Ball { get; set; }
+        Platform Platform { get; set; }
+    }
+}
