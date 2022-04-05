9fba1a4b10b518a634d445ee686904071f87e8fb
Nagy Patrik <nagy.patrik024@gmail.com>
2022-04-05 12:15:19 +0200

    display inprogress

--- Pong/MainWindow.xaml.cs
+++ Pong/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Pong
 {
@@ -20,9 +21,54 @@ namespace Pong
     /// </summary>
     public partial class MainWindow : Window
     {
+        //AsteroidsLogic logic;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if (e.Key == Key.Left)
+            //{
+            //    logic.Control(AsteroidsLogic.Controls.Left);
+            //}
+            //else if (e.Key == Key.Right)
+            //{
+            //    logic.Control(AsteroidsLogic.Controls.Right);
+            //}
+        }
+        private void Dt_Tick(object? sender, EventArgs e)
+        {
+            //logic.TimeStep();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //logic = new AsteroidsLogic();
+            //logic.GameOver += Logic_GameOver;
+            //display.SetupModel(logic);
+
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromMilliseconds(100);
+            dt.Tick += Dt_Tick;
+            dt.Start();
+
+            display.SetupSizes(new Size(grid.ActualWidth, grid.ActualHeight));
+            //logic.SetupSizes(new System.Windows.Size((int)grid.ActualWidth, (int)grid.ActualHeight));
+        }
+        private void Logic_GameOver(object? sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Game Over");
+            if (result == MessageBoxResult.OK)
+            {
+                this.Close();
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Pong/Renderer/Display.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Pong.Renderer
+{
+    class Display : FrameworkElement
+    {
+        Size area;
+        //IGameModel model;
+        public void SetupSizes(Size area)
+        {
+            this.area = area;
+            this.InvalidateVisual();
+        }
+
+        //public void SetupModel(IGameModel model)
+        //{
+        //    this.model = model;
+        //    this.model.Changed += (sender, eventargs) => this.InvalidateVisual();
+        //}
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            if (area.Width > 0 && area.Height > 0 /*&& model != null*/)
+            {
+                drawingContext.DrawRectangle(Brushes.Black, new Pen(Brushes.Black, 0),
+                  new Rect(0, 0, area.Width, area.Height));
+
+                drawingContext.PushTransform(new TranslateTransform((double)100, (double)0));
+                drawingContext.Pop();
+
+                
+            }
+
+        }
+    }
+}
